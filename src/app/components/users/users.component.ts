41962c9006a7e9bc632bdf3de7e95bf8f994f0ec
the-markussohn <markus.justinas@gmail.com>
2018-08-19 23:26:04 +0300

    ngif

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,54 +9,54 @@ import {User} from '../../models/User';
 export class UsersComponent implements OnInit {
 
   users: User[];
+  showExtended = true;
+  loaded: boolean;
 
   constructor() {
   }
 
   ngOnInit() {
-    this.users = [
-      {
-        firstName: 'John',
-        lastName: 'Smith',
-        age: 30,
-        address: {
-          street: '50 Main st',
-          city: 'Boston',
-          state: 'MA'
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Smith',
+          age: 30,
+          address: {
+            street: '50 Main st',
+            city: 'Boston',
+            state: 'MA'
+          }
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Johnson',
+          age: 36,
+          address: {
+            street: '20 School st',
+            city: 'Lynn',
+            state: 'MA'
+          }
+        },
+        {
+          firstName: 'Karen',
+          lastName: 'Brooks',
+          age: 29,
+          address: {
+            street: '55 Mill st',
+            city: 'Miami',
+            state: 'FL'
+          }
         }
-      },
-      {
-        firstName: 'Kevin',
-        lastName: 'Johnson',
-        age: 36,
-        address: {
-          street: '20 School st',
-          city: 'Lynn',
-          state: 'MA'
-        }
-      },
-      {
-        firstName: 'Karen',
-        lastName: 'Brooks',
-        age: 29,
-        address: {
-          street: '55 Mill st',
-          city: 'Miami',
-          state: 'FL'
-        }
-      }
-    ];
+      ];
 
-    this.addUser({
-      firstName: 'David',
-      lastName: 'Lynch',
-      age: 44,
-      address: {
-        street: '12 Wake st',
-        city: 'Miami',
-        state: 'FL'
-      }
-    });
+      this.loaded = true;
+
+      this.addUser({
+        firstName: 'David',
+        lastName: 'Lynch'
+      });
+    }, 2000);
   }
 
   addUser(user: User) {
